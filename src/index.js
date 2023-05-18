0618ee16e2368d3c97b553a2530241df7a3ac560
stailz <stailzsanchez@gmail.com>
2023-05-18 17:45:06 +0300

    config @apollo/server_v4 - ok

--- src/index.js
+++ src/index.js
@@ -1,27 +1,71 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("@apollo/server");
 const connectDB = require("./config/db");
 const typeDefs = require("./types");
 const resolvers = require("./resolvers");
 const models = require("./models");
 const authContext = require("./middleware/auth");
-// import { createServer } from "http";
-// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-// import { makeExecutableSchema } from "@graphql-tools/schema";
-// import { WebSocketServer } from "ws";
-// import { useServer } from "graphql-ws/lib/use/ws";
-connectDB();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: { models, authContext },
-  csrfPrevention: true,
-  cors: {
-    origin: "*",
-    credentials: true,
-  },
-});
+(async function () {
+  const { createServer } = require("http");
+  const {
+    ApolloServerPluginDrainHttpServer,
+  } = require("@apollo/server/plugin/drainHttpServer");
+  const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`Server is ready at ${url}`);
-});
+  const { WebSocketServer } = require("ws");
+  const { useServer } = require("graphql-ws/lib/use/ws");
+  const { expressMiddleware } = require("@apollo/server/express4");
+
+  connectDB();
+
+  const app = express();
+  const httpServer = createServer(app);
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  console.log("models", models);
+
+  const server = new ApolloServer({
+    schema,
+    context: { models },
+    csrfPrevention: true,
+    cors: {
+      origin: "*",
+      credentials: true,
+    },
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  // server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  //   console.log(`Server is ready at ${url}`);
+  // });
+
+  await server.start();
+  app.use("/graphql", cors(), bodyParser.json(), expressMiddleware(server));
+
+  const PORT = 4000;
+  // Now that our HTTP server is fully set up, we can listen to it.
+  httpServer.listen(PORT, () => {
+    console.log(`Server run http://localhost:${PORT}/graphql`);
+  });
+})();
--- src/resolvers/queries/getUsers.js
+++ src/resolvers/queries/getUsers.js
@@ -1,6 +1,8 @@
 const { transformGame, transformRound, transformUser } = require("../merge");
 
-module.exports = async (_, {}, { models }) => {
+module.exports = async (_, {}, { models }, { tmp }) => {
+  console.log("models", models);
+  console.log("tmp", tmp);
   try {
     const users = await models.User.find({});
     return users.map((user) => transformUser(user));
